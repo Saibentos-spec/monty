195f241b4ad400236d6d3b55ced5688a90b9063f
SAIBA MOISE <saibentos@gmail.com>
2026-06-01 11:55:41 +0200

    Create pall.c

---
+++ pall.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+void pall(stack_t **stack, unsigned int line_number)
+{
+    stack_t *tmp = *stack;
+    (void)line_number;
+
+    while (tmp)
+    {
+        printf("%d\n", tmp->n);
+        tmp = tmp->next;
+    }
+}
