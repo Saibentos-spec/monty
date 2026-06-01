035109827899a62df6aead794a98cedfa2e73aa3
SAIBA MOISE <saibentos@gmail.com>
2026-06-01 13:18:31 +0200

    Update pall.c

--- pall.c
+++ pall.c
@@ -1,13 +1,21 @@
 #include "monty.h"
 
+/**
+ * pall - prints all stack elements
+ * @stack: stack pointer
+ * @line_number: line number
+ *
+ * Return: void
+ */
 void pall(stack_t **stack, unsigned int line_number)
 {
-    stack_t *tmp = *stack;
-    (void)line_number;
+	stack_t *tmp = *stack;
 
-    while (tmp)
-    {
-        printf("%d\n", tmp->n);
-        tmp = tmp->next;
-    }
+	(void)line_number;
+
+	while (tmp)
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+	}
 }
