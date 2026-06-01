0ad1056df74f03e7d12ba6c52df8cd72e2c5b22c
SAIBA MOISE <saibentos@gmail.com>
2026-06-01 11:53:11 +0200

    Create monty.h

---
+++ monty.h
@@ -0,0 +1,31 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+typedef struct stack_s
+{
+    int n;
+    struct stack_s *prev;
+    struct stack_s *next;
+} stack_t;
+
+/* Core functions */
+void push(stack_t **stack, unsigned int line_number, char *arg);
+void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+void _div(stack_t **stack, unsigned int line_number);
+void mul(stack_t **stack, unsigned int line_number);
+
+/* Helpers */
+int is_number(char *s);
+void free_stack(stack_t *stack);
+
+#endif
