c162242ca15a21ab289b3c67ca0e18cd2af0980f
SAIBA MOISE <saibentos@gmail.com>
2026-06-01 12:01:21 +0200

    Update push.c

--- push.c
+++ push.c
@@ -1,26 +1,11 @@
-void push(stack_t **stack, unsigned int line_number, char *arg)
+int is_number(char *s)
 {
-    stack_t *new_node;
-    int value;
-
-    if (!arg || is_number(arg) == 0)
-    {
-        fprintf(stderr, "L%u: usage: push integer\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-
-    value = atoi(arg);
-
-    new_node = malloc(sizeof(stack_t));
-    if (!new_node)
-        exit(EXIT_FAILURE);
-
-    new_node->n = value;
-    new_node->prev = NULL;
-    new_node->next = *stack;
-
-    if (*stack)
-        (*stack)->prev = new_node;
-
-    *stack = new_node;
+    if (!s || *s == '\0')
+        return 0;
+    if (*s == '-' || *s == '+')
+        s++;
+    for (; *s; s++)
+        if (!isdigit(*s))
+            return 0;
+    return 1;
 }
