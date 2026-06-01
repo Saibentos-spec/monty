c9207432cda5c962282f55bc0c910f396a2568c7
SAIBA MOISE <saibentos@gmail.com>
2026-06-01 11:54:27 +0200

    Create opcode_dispatch.c

---
+++ opcode_dispatch.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+void get_op_func(char *opcode, stack_t **stack,
+                 unsigned int line_number, char *arg)
+{
+    if (strcmp(opcode, "push") == 0)
+        push(stack, line_number, arg);
+    else if (strcmp(opcode, "pall") == 0)
+        pall(stack, line_number);
+    else if (strcmp(opcode, "pint") == 0)
+        pint(stack, line_number);
+    else if (strcmp(opcode, "pop") == 0)
+        pop(stack, line_number);
+    else if (strcmp(opcode, "swap") == 0)
+        swap(stack, line_number);
+    else if (strcmp(opcode, "add") == 0)
+        add(stack, line_number);
+    else if (strcmp(opcode, "nop") == 0)
+        nop(stack, line_number);
+    else if (strcmp(opcode, "div") == 0)
+        _div(stack, line_number);
+    else if (strcmp(opcode, "mul") == 0)
+        mul(stack, line_number);
+    else
+    {
+        fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
+        exit(EXIT_FAILURE);
+    }
+}
