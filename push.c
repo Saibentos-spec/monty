239f6dde74cb62e8ec67b5b65eefc04679eb878b
SAIBA MOISE <saibentos@gmail.com>
2026-06-01 11:59:43 +0200

    Update push.c

--- push.c
+++ push.c
@@ -1,40 +1,26 @@
-#include "monty.h"
-
-int is_number(char *s)
-{
-    if (!s) return 0;
-    if (*s == '-' || *s == '+') s++;
-    if (!*s) return 0;
-
-    while (*s)
-    {
-        if (!isdigit(*s))
-            return 0;
-        s++;
-    }
-    return 1;
-}
-
 void push(stack_t **stack, unsigned int line_number, char *arg)
 {
-    stack_t *new;
+    stack_t *new_node;
+    int value;
 
-    if (!arg || !is_number(arg))
+    if (!arg || is_number(arg) == 0)
     {
         fprintf(stderr, "L%u: usage: push integer\n", line_number);
         exit(EXIT_FAILURE);
     }
 
-    new = malloc(sizeof(stack_t));
-    if (!new)
+    value = atoi(arg);
+
+    new_node = malloc(sizeof(stack_t));
+    if (!new_node)
         exit(EXIT_FAILURE);
 
-    new->n = atoi(arg);
-    new->prev = NULL;
-    new->next = *stack;
+    new_node->n = value;
+    new_node->prev = NULL;
+    new_node->next = *stack;
 
     if (*stack)
-        (*stack)->prev = new;
+        (*stack)->prev = new_node;
 
-    *stack = new;
+    *stack = new_node;
 }
