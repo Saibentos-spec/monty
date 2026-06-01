179aab6f00616deb09fd24b8e0cf19bfdbbaaf4e
SAIBA MOISE <saibentos@gmail.com>
2026-06-01 13:21:55 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,46 +1,48 @@
 #include "monty.h"
 
 /**
- * main - Monty interpreter entry point
+ * main - Monty bytecode interpreter
  * @ac: argument count
  * @av: argument vector
  *
  * Return: EXIT_SUCCESS or EXIT_FAILURE
  */
-
 int main(int ac, char **av)
 {
-    FILE *file;
-    char line[256];
-    unsigned int line_number = 0;
-    stack_t *stack = NULL;
+	FILE *file;
+	char line[256];
+	char *opcode, *arg;
+	unsigned int line_number = 0;
+	stack_t *stack = NULL;
 
-    if (ac != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
+	if (ac != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
 
-    file = fopen(av[1], "r");
-    if (!file)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", av[1]);
-        exit(EXIT_FAILURE);
-    }
+	file = fopen(av[1], "r");
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", av[1]);
+		exit(EXIT_FAILURE);
+	}
 
-    while (fgets(line, sizeof(line), file))
-    {
-        char *opcode = strtok(line, " \n\t");
-        char *arg = strtok(NULL, " \n\t");
-        line_number++;
+	while (fgets(line, sizeof(line), file))
+	{
+		line_number++;
 
-        if (!opcode || opcode[0] == '#')
-            continue;
+		opcode = strtok(line, " \t\n");
+		arg = strtok(NULL, " \t\n");
 
-        get_op_func(opcode, &stack, line_number, arg);
-    }
+		if (!opcode || opcode[0] == '#')
+			continue;
 
-    free_stack(stack);
-    fclose(file);
-    return 0;
+		get_op_func(opcode, &stack, line_number, arg);
+	}
+
+	free_stack(stack);
+	fclose(file);
+
+	return (EXIT_SUCCESS);
 }
