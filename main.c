6bd005fec32c51e2ab37a6716871884184712de2
SAIBA MOISE <saibentos@gmail.com>
2026-06-01 12:37:24 +0200

    Update main.c

--- main.c
+++ main.c
@@ -20,6 +20,11 @@ int main(int ac, char **av)
         exit(EXIT_FAILURE);
     }
 
+      if (!file)
+                 {
+	  return;
+               }
+
     while (fgets(line, sizeof(line), file))
     {
         char *opcode = strtok(line, " \n\t");
