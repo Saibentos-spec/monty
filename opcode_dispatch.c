e3441bf0ce4911c98890a847351575e8caa001ec
SAIBA MOISE <saibentos@gmail.com>
2026-06-14 14:26:51 +0200

    Update opcode_dispatch.c

--- opcode_dispatch.c
+++ opcode_dispatch.c
@@ -25,6 +25,8 @@ void get_op_func(char *opcode, stack_t **stack,
 	    sub(stack, line_number);
     else if (strcmp(opcode, "mod") == 0)
 	    mod(stack, line_number);
+	else if (strcmp(opcode, "pchar") == 0)
+	pchar(stack, line_number);
     else
     {
         fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
