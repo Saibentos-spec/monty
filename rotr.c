916d2c1c72ace2d448859388cb01ffe9bbf4411e
SAIBA MOISE <saibentos@gmail.com>
2026-06-14 14:37:35 +0200

    Create rotr.c

---
+++ rotr.c
@@ -0,0 +1,29 @@
+#include "monty.h"
+
+/**
+ * rotr - rotates stack right
+ * @stack: stack head
+ * @line_number: line number
+ */
+void rotr(stack_t **stack, unsigned int line_number)
+{
+	stack_t *last;
+
+	(void)line_number;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+		return;
+
+	last = *stack;
+
+	while (last->next)
+		last = last->next;
+
+	last->prev->next = NULL;
+
+	last->prev = NULL;
+	last->next = *stack;
+
+	(*stack)->prev = last;
+	*stack = last;
+}
