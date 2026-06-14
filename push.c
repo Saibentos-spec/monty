487100ef74960279c1f565997e6e57cccba09758
SAIBA MOISE <saibentos@gmail.com>
2026-06-14 17:06:16 +0200

    Update push.c

--- push.c
+++ push.c
@@ -1,33 +1,40 @@
 #include "monty.h"
 
-/**
- * push - pushes element to stack
- * @stack: stack pointer
- * @line_number: line number
- * @arg: integer argument
- *
- * Return: void
- */
+int is_number(char *s)
+{
+    if (!s) return 0;
+    if (*s == '-' || *s == '+') s++;
+    if (!*s) return 0;
+
+    while (*s)
+    {
+        if (!isdigit(*s))
+            return 0;
+        s++;
+    }
+    return 1;
+}
+
 void push(stack_t **stack, unsigned int line_number, char *arg)
 {
-	stack_t *new;
+    stack_t *new;
 
-	if (!arg || !is_number(arg))
-	{
-		fprintf(stderr, "L%u: usage: push integer\n", line_number);
-		exit(EXIT_FAILURE);
-	}
+    if (!arg || !is_number(arg))
+    {
+        fprintf(stderr, "L%u: usage: push integer\n", line_number);
+        exit(EXIT_FAILURE);
+    }
 
-	new = malloc(sizeof(stack_t));
-	if (!new)
-		exit(EXIT_FAILURE);
+    new = malloc(sizeof(stack_t));
+    if (!new)
+        exit(EXIT_FAILURE);
 
-	new->n = atoi(arg);
-	new->prev = NULL;
-	new->next = *stack;
+    new->n = atoi(arg);
+    new->prev = NULL;
+    new->next = *stack;
 
-	if (*stack)
-		(*stack)->prev = new;
+    if (*stack)
+        (*stack)->prev = new;
 
-	*stack = new;
+    *stack = new;
 }
