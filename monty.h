409501ad397faa6d2ddfd96a1536387c4d5f8cdb
SAIBA MOISE <saibentos@gmail.com>
2026-06-14 14:37:00 +0200

    Update monty.h

--- monty.h
+++ monty.h
@@ -28,7 +28,7 @@ void mod(stack_t **stack, unsigned int line_number);
 void pchar(stack_t **stack, unsigned int line_number);
 void pstr(stack_t **stack, unsigned int line_number);
 void rotl(stack_t **stack, unsigned int line_number);
-
+void rotr(stack_t **stack, unsigned int line_number);
 /* Helpers */
 int is_number(char *s);
 void free_stack(stack_t *stack);
